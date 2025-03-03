2cef6f801389e6f45826a51bba1e2a552b36db9c
Artymiik <aworkprogramm@gmail.com>
2025-03-03 23:41:39 +0500

    added formated and pre-commit build for scala language

--- server/broker-service/build.sbt
+++ server/broker-service/build.sbt
@@ -16,4 +16,6 @@ scalacOptions ++= Seq("-encoding", "UTF-8")
 
 libraryDependencies += "org.postgresql" % "postgresql" % "42.5.4"
 libraryDependencies += "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.14.0"
+
+enablePlugins(ScalafmtPlugin)
 // See https://www.scala-sbt.org/1.x/docs/Using-Sonatype.html for instructions on how to publish to Sonatype.
--- server/gateway-service/src/main/scala/Networking/ClientHandler.scala
+++ server/gateway-service/src/main/scala/Networking/ClientHandler.scala
@@ -6,31 +6,49 @@ import akka.http.scaladsl.Http
 import akka.stream.Materializer
 import akka.http.scaladsl.settings.ConnectionPoolSettings
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 import scala.concurrent.duration._
 
 import Packages.Logger
 
 object ClientHandler {
-    def forward_to_backend(request: HttpRequest, targetURL: String)(implicit system: ActorSystem, mat: Materializer, ec: ExecutionContext): Future[HttpResponse] = {
-        val clientIP = request.headers.find(_.name == "X-Forwarded-For").map(_.value).getOrElse(request.uri.authority.host.address())
-        Logger.logServer(clientIP, request.method.value, request.uri.path.toString(), "200 (Forwarded)", s"We forward the request to the microservice: $targetURL")
+    def forward_to_backend(request: HttpRequest, targetURL: String)(implicit
+        system: ActorSystem,
+        mat: Materializer,
+        ec: ExecutionContext
+    ): Future[HttpResponse] = {
+        val clientIP = request.headers
+            .find(_.name == "X-Forwarded-For")
+            .map(_.value)
+            .getOrElse(request.uri.authority.host.address())
+        Logger.logServer(
+          clientIP,
+          request.method.value,
+          request.uri.path.toString(),
+          "200 (Forwarded)",
+          s"We forward the request to the microservice: $targetURL"
+        )
 
         val filteredHeaders = request.headers.filterNot(_.name == "Timeout-Access")
 
         val pool = ConnectionPoolSettings(system)
             .withIdleTimeout(3.seconds)
 
-        Http().singleRequest(HttpRequest(
-            method = request.method,
-            uri = targetURL,
-            headers = request.headers,
-            entity = request.entity
-        ),
-        settings = pool
-        ).recover {
-            case ex: Exception =>
-                HttpResponse(StatusCodes.InternalServerError, entity = "Failed to send request to microservice")
-        }
+        Http()
+            .singleRequest(
+              HttpRequest(
+                method = request.method,
+                uri = targetURL,
+                headers = request.headers,
+                entity = request.entity
+              ),
+              settings = pool
+            )
+            .recover { case ex: Exception =>
+                HttpResponse(
+                  StatusCodes.InternalServerError,
+                  entity = "Failed to send request to microservice"
+                )
+            }
     }
 }
