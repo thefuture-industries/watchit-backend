c790d7e3640a162459993ab766f06aef1181a63d
Артём Власов <aworkprogramm@gmail.com>
2025-05-05 17:26:18 +0500

    changed server/user-service/go.mod, server/user-service/go.sum, server/user-service/internal/common/utils/json.go

--- server/user-service/internal/common/utils/json.go
+++ server/user-service/internal/common/utils/json.go
@@ -1,16 +1,17 @@
-// *---------------------------------------------------------------------------------------------
-//  *  Copyright (c). All rights reserved.
-//  *  Licensed under the MIT License. See License.txt in the project root for license information.
-//  *--------------------------------------------------------------------------------------------*
-
 package utils
 
 import (
-	"encoding/json"
+	"compress/gzip"
 	"fmt"
-	"go-user-service/internal/packages"
+	"go-user-service/internal/lib"
+	"io"
 	"net/http"
+	"strings"
+
+	jsoniter "github.com/json-iterator/go"
 )
+
+var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
 // ---------------------------------------
 // Проверка и декодирование данных от user
@@ -26,20 +27,35 @@ func ParseJSON(r *http.Request, payload any) error {
 // ---------------------------
 // Функция ответа пользователю
 // ---------------------------
-func WriteJSON(w http.ResponseWriter, status int, v any) {
+func WriteJSON(w http.ResponseWriter, r *http.Request, status int, v any) {
+	logger := lib.NewLogger()
+
+	accept := r.Header.Get("Accept-Encoding")
+	shouldGzip := strings.Contains(accept, "gzip")
+
+	var writer io.Writer = w
+	var gzipWriter *gzip.Writer
+
+	if shouldGzip {
+		w.Header().Set("Content-Encoding", "gzip")
+		w.Header().Set("Vary", "Accept-Encoding")
+
+		gzipWriter = gzip.NewWriter(w)
+		defer gzipWriter.Close()
+
+		writer = gzipWriter
+	}
+
 	w.Header().Set("Content-Security-Policy", "script-src 'self';")
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(status)
 
-	if err := json.NewEncoder(w).Encode(map[string]any{"message": v}); err != nil {
-		packages.ErrorLog(err)
-		http.Error(w, "Неизвестная ошибка от сервера", http.StatusBadGateway)
+	if err := json.NewEncoder(writer).Encode(map[string]any{"message": v}); err != nil {
+		logger.Error(err.Error())
+		http.Error(w, "Unknown error from the server", http.StatusBadGateway)
 	}
 }
 
-// --------------------------------
-// Функция обработки ошибок сервера
-// --------------------------------
-func WriteError(w http.ResponseWriter, status int, err error) {
-	WriteJSON(w, status, err.Error())
+func CacheJSON(w http.ResponseWriter, limit int) {
+	w.Header().Set("Cache-Control", fmt.Sprintf("public, max-age=%d", limit))
 }
