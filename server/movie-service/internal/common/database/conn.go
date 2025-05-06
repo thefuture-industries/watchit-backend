4764635e3994209af5b8ec62c7de3d3ae6c6f34a
Артём Власов <aworkprogramm@gmail.com>
2025-05-06 15:28:17 +0500

    including server/movie-service/internal/common/database/schema.go | changed server/movie-service/internal/common/database/conn.go, server/movie-service/internal/common/database/schema.go

--- server/movie-service/internal/common/database/conn.go
+++ server/movie-service/internal/common/database/conn.go
@@ -5,6 +5,7 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var db *gorm.DB
@@ -13,19 +14,21 @@ func ConnectDB(dsn string) {
 	loggerApp := lib.NewLogger()
 
 	var err error
-	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Discard,
+	})
 	if err != nil {
 		loggerApp.Error(err.Error())
 		return
 	}
 
-	// err = db.AutoMigrate(&Users{}, &Payments{})
-	// if err != nil {
-	// 	loggerApp.Error(err.Error())
-	// 	return
-	// }
+	err = db.AutoMigrate(&Recommendations{}, &Genres{})
+	if err != nil {
+		loggerApp.Error(err.Error())
+		return
+	}
 
-	// loggerApp.Info("Migration completed successfully.")
+	loggerApp.Info("Migration completed successfully.")
 	loggerApp.Info("Successfully connected to database!")
 }
 
---
+++ server/movie-service/internal/common/database/schema.go
@@ -0,0 +1,17 @@
+package database
+
+import "time"
+
+type Genres struct {
+	ID        uint `gorm:"primarykey" json:"id"`
+	GenreID   uint `gorm:"unique;not null" json:"genre_id"`
+	GenreName uint `gorm:"unique;not null" json:"genre_name"`
+}
+
+type Recommendations struct {
+	ID        uint      `gorm:"primarykey" json:"id"`
+	UUID      string    `gorm:"unique;size:255;not null" json:"uuid"`
+	GenreID   uint      `gorm:"not null" json:"genre_id"`
+	Count     uint      `gorm:"not null" json:"count"`
+	CreatedAt time.Time `json:"created_at"`
+}
