f2e04a27306625a126db6877c679e60f3c42f348
Артём Власов <aworkprogramm@gmail.com>
2025-05-08 00:48:22 +0500

    changed server/movie-service/internal/common/database/action/recommendation.go, server/movie-service/internal/lib/movie/idx.go, server/movie-service/internal/lib/movie/movie.go, server/movie-service/internal/types/types.go

--- server/movie-service/internal/lib/movie/idx.go
+++ server/movie-service/internal/lib/movie/idx.go
@@ -8,6 +8,8 @@ import (
 	"go-movie-service/internal/lib"
 	"go-movie-service/internal/types"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func MovieIDX() {
@@ -45,4 +47,23 @@ func MovieIDX() {
 	for page, offset := range index {
 		fmt.Fprintf(idxFile, "%d %d\n", page, offset)
 	}
+}
+
+func LoadIDX() map[uint32]uint32 {
+	idx := make(map[uint32]uint32)
+
+	file, _ := os.Open(constants.MOVIE_IDX_PATH)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		parts := strings.Fields(scanner.Text())
+		if len(parts) == 2 {
+			page, _ := strconv.ParseUint(parts[0], 10, 32)
+			offset, _ := strconv.ParseUint(parts[1], 10, 32)
+			idx[uint32(page)] = uint32(offset)
+		}
+	}
+
+	return idx
 }
--- server/movie-service/internal/lib/movie/movie.go
+++ server/movie-service/internal/lib/movie/movie.go
@@ -1,5 +1,24 @@
 package movie
 
-func Movie() {
+import (
+	"math/rand"
+	"time"
+)
 
+type Movie struct{}
+
+func NewMovie() *Movie {
+	return &Movie{}
+}
+
+func (m *Movie) GetMovies() {
+	index := LoadIDX()
+
+	rand.Seed(time.Now().UnixNano())
+	var pages []uint
+	for p := range index {
+		if p <= 500 {
+			pages = append(pages, uint(p))
+		}
+	}
 }
