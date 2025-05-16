57297ff53ba5c698a778f2f273cc52e4aac8a181
Артём Власов <aworkprogramm@gmail.com>
2025-05-16 11:44:56 +0500

    changed parameter types: genreID in CreateRecommendation function, genreID in UpdateRecommendation function

--- server/movie-service/cmd/api/api.go
+++ server/movie-service/cmd/api/api.go
@@ -44,9 +44,7 @@ func (s *APIServer) Run() error {
 		http.ServeFile(w, r, "./docs/vision/index.html")
 	}).Methods("GET")
 
-	// ------
 	// Scalar
-	// ------
 	subrouter.PathPrefix("/docs/").Handler(http.StripPrefix("/docs/", http.FileServer(http.Dir("./docs"))))
 	subrouter.HandleFunc("/docs", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./docs/swagger.json")
@@ -68,32 +66,22 @@ func (s *APIServer) Run() error {
 		fmt.Fprintln(w, htmlContent)
 	})
 
-	// -------------
 	// ROUTERS PATHS
-	// -------------
 	errors := packages.NewErrors(monitoring)
 
 	recommendation.NewHandler(monitoring, errors).RegisterRoutes(subrouter)
-	// sync.RegisterRoutes(subrouter)
-
 	movie.NewHandler(monitoring, errors).MovieRoutes(subrouter)
 
-	// ------------------
 	// Логирование server
-	// ------------------
 	router.Use(packages.NewLogger().LoggerMiddleware)
 
-	// ---------------
 	// подключаем CORS
-	// ---------------
 	origins := handlers.AllowedOrigins([]string{"*"})
 	methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"})
 	headers := handlers.AllowedHeaders([]string{"Content-Type", "X-Requested-With", "Authorization"})
 	allowCredentials := handlers.AllowCredentials()
 
-	// --------------------
 	// Возращяем http ответ
-	// --------------------
 	s.logger.Info(fmt.Sprintf("Listening on %s", s.addr))
 	return http.ListenAndServe(s.addr, handlers.CORS(origins, methods, headers, allowCredentials)(router))
 }
