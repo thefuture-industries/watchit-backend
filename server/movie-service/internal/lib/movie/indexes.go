55395d9d4962c6a63f07d8fa3eeddf841ff1d103
Артём Власов <aworkprogramm@gmail.com>
2025-05-22 23:14:46 +0500

    added variable decoder | deleted functions: Pidx, LoadPIDX

--- server/movie-service/internal/lib/movie/indexes.go
+++ server/movie-service/internal/lib/movie/indexes.go
@@ -1,75 +1,28 @@
 package movie
 
 import (
-	"bufio"
-	"encoding/json"
-	"fmt"
-	"go-movie-service/internal/common/constants"
-	"go-movie-service/internal/lib"
-	"go-movie-service/internal/types"
 	"io"
 	"os"
-	"strconv"
-	"strings"
 )
 
-func Pidx(page uint16) {
-	logger := lib.NewLogger()
+func getPageByOffset(filePath string, offset int64) (Page, error) {
+	var page Page
 
-	file, err := os.Open(constants.MOVIE_JSON_PATH_WRITE)
+	f, err := os.Open(filePath)
 	if err != nil {
-		logger.Error(err.Error())
-		return
+		return page, err
 	}
-	defer file.Close()
+	defer f.Close()
 
-	decoder := json.NewDecoder(bufio.NewReader(file))
-
-	token, err := decoder.Token()
-	if err != nil || token != json.Delim('[') {
-		logger.Error("Not an array")
-		return
-	}
-
-	index := make(map[int]uint64)
-	for decoder.More() {
-		offset, _ := file.Seek(0, io.SeekCurrent)
-		var page types.Movies
-		if err := decoder.Decode(&page); err != nil {
-			logger.Error(err.Error())
-			return
-		}
-
-		index[int(page.Page)] = uint64(offset)
-	}
-
-	idxFile, _ := os.Create(constants.MOVIE_PIDX_PATH_WRITE)
-	defer idxFile.Close()
-	for page, offset := range index {
-		fmt.Fprintf(idxFile, "%d %d\n", page, offset)
-	}
-}
-
-func LoadPIDX() map[uint32]uint32 {
-	logger := lib.NewLogger()
-	idx := make(map[uint32]uint32)
-
-	file, err := os.Open(constants.MOVIE_PIDX_PATH_WRITE)
+	_, err = f.Seek(offset, io.SeekStart)
 	if err != nil {
-		logger.Error(err.Error())
-		return nil
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		parts := strings.Fields(scanner.Text())
-		if len(parts) == 2 {
-			page, _ := strconv.ParseUint(parts[0], 10, 32)
-			offset, _ := strconv.ParseUint(parts[1], 10, 32)
-			idx[uint32(page)] = uint32(offset)
-		}
+		return page, err
 	}
 
-	return idx
+	decoder := json.NewDecoder(f)
+	if err := decoder.Decode(&page); err != nil {
+		return page, err
+	}
+
+	return page, nil
 }
