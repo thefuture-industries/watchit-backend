0c374dd17451f6574c5df9808b948567a9e1f7bb
Артём Власов <aworkprogramm@gmail.com>
2025-06-11 23:54:26 +0500

    deleted structure Recommendations | deleted type Recommendations

--- server/movie-service/internal/common/database/schema/schema.go
+++ server/movie-service/internal/common/database/schema/schema.go
@@ -11,22 +11,14 @@ type Genres struct {
 	GenreName string `gorm:"unique;not null" json:"genre_name"`
 }
 
-type Recommendations struct {
-	ID        uint      `gorm:"primarykey" json:"id"`
-	UUID      string    `gorm:"size:255;not null" json:"uuid"`
-	GenreID   uint      `gorm:"not null" json:"genre_id"`
-	Count     uint      `gorm:"not null" json:"count"`
-	CreatedAt time.Time `json:"created_at"`
-}
-
 type Users struct {
 	ID           uint           `gorm:"primarykey" json:"id"`
-	UUID         string         `gorm:"unique;size:255;not null" json:"uuid"`
-	Username     string         `gorm:"unique;size:20;not null" json:"username"`
+	UUID         string         `gorm:"unique;type:varchar(255);not null" json:"uuid"`
+	Username     string         `gorm:"unique;type:varchar(20);not null" json:"username"`
 	Email        sql.NullString `gorm:"unique;type:varchar(100);default:null" json:"email"`
-	Password     string         `gorm:"size:50;not null" json:"password"`
-	IPAddress    string         `gorm:"size:15;not null" json:"ip_address"`
-	Country      string         `gorm:"size:50;not null" json:"country"`
+	Password     string         `gorm:"type:varchar(50);not null" json:"password"`
+	IPAddress    string         `gorm:"type:varchar(15);not null" json:"ip_address"`
+	Country      string         `gorm:"type:varchar(50);not null" json:"country"`
 	Subscription bool           `gorm:"default:false;not null" json:"subscription"`
 	CreatedAt    time.Time      `json:"created_at"`
 	UpdatedAt    time.Time      `json:"updated_at"`
@@ -34,8 +26,8 @@ type Users struct {
 
 type Profiles struct {
 	ID        uint      `gorm:"primarykey" json:"id"`
-	UserID    uint      `gorm:"unique;not null" json:"user_id"`
-	Bio       string    `gorm:"size:255;" json:"bio"`
+	UUID      string    `gorm:"unique;type:varchar(255);not null" json:"uuid"`
+	Bio       string    `gorm:"type:varchar(255)" json:"bio"`
 	Avatar    byte      `json:"avatar"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
@@ -43,7 +35,7 @@ type Profiles struct {
 
 type Payments struct {
 	ID          uint      `gorm:"primarykey" json:"id"`
-	UserID      uint      `gorm:"not null" json:"user_id"`
+	UUID        string    `gorm:"unique;type:varchar(255);not null" json:"uuid"`
 	Amount      float64   `gorm:"not null" json:"amount"`
 	PaymentDate time.Time `gorm:"not null" json:"payment_date"`
 	CreatedAt   time.Time `json:"created_at"`
@@ -51,28 +43,30 @@ type Payments struct {
 }
 
 type PaymentCards struct {
-	ID     uint `gorm:"primarykey" json:"id"`
-	UserID uint `gorm:"not null" json:"user_id"`
-	UserID uint `gorm:"not null" json:"user_id"`
-	UserID uint `gorm:"not null" json:"user_id"`
-	UserID uint `gorm:"not null" json:"user_id"`
-	UserID uint `gorm:"not null" json:"user_id"`
+	ID               uint      `gorm:"primarykey" json:"id"`
+	UUID             string    `gorm:"unique;type:varchar(255);not null" json:"uuid"`
+	CardNumber       string    `gorm:"type:varchar(16);not null" json:"card_number"`
+	ExpirationMounth int16     `gorm:"not null;check:expiration_month >= 1 AND expiration_month <= 12" json:"expiration_mounth"`
+	ExpirationYear   int16     `gorm:"not null" json:"expiration_year"`
+	CVVCode          string    `gorm:"type:varchar(3);not null" json:"cvv_code"`
+	CardHolderName   string    `gorm:"type:varchar(100);not null" json:"cardholder_name"`
+	CreatedAt        time.Time `json:"created_at"`
+	UpdatedAt        time.Time `json:"updated_at"`
 }
 
 type Limiter struct {
 	ID           uint      `gorm:"primarykey" json:"id"`
-	UUID         string    `gorm:"unique;size:255;not null" json:"uuid"`
+	UUID         string    `gorm:"unique;type:varchar(255);not null" json:"uuid"`
 	TextLimit    uint      `gorm:"default:3;not null;" json:"text_limit"`
-	YoutubeLimit uint      `gorm:"default:2;not null" json:"youtube_limit"`
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
 }
 
 type Favourites struct {
 	ID          uint      `gorm:"primarykey" json:"id"`
-	UUID        string    `gorm:"unique;size:255;not null" json:"uuid"`
+	UUID        string    `gorm:"unique;type:varchar(255);not null" json:"uuid"`
 	MovieID     uint      `gorm:"not null" json:"movie_id"`
-	MoviePoster string    `gorm:"size:255;not null" json:"movie_poster"`
+	MoviePoster string    `gorm:"type:varchar(255);not null" json:"movie_poster"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
