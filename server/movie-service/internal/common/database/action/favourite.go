f83a0657001e8f9fffb8f233976d49ab5a1ff184
Артём Власов <aworkprogramm@gmail.com>
2025-06-11 23:40:45 +0500

    added variables: logger, db, genres, recomSchemaPrepare | added function CreateFavourite | err := db.Where("genre_id = ?", genreID).First(&genres).Error; if err is not equal to nil; err := db.Create(&recomSchemaPrepare).Error; if err is not equal to nil

--- server/movie-service/internal/common/database/action/favourite.go
+++ server/movie-service/internal/common/database/action/favourite.go
@@ -1,3 +1,25 @@
 package action
 
+func CreateFavourite(uuid string, genreID uint16) error {
+	logger := lib.NewLogger()
+	db := database.GetDB()
 
+	var genres schema.Genres
+	if err := db.Where("genre_id = ?", genreID).First(&genres).Error; err != nil {
+		logger.Error(fmt.Sprintf("Database -> 'Failed to create recommendation: %v'", err))
+		return fmt.Errorf("failed to create recommendation.")
+	}
+
+	recomSchemaPrepare := schema.Recommendations{
+		UUID:    uuid,
+		GenreID: genres.ID,
+		Count:   1,
+	}
+
+	if err := db.Create(&recomSchemaPrepare).Error; err != nil {
+		logger.Error(fmt.Sprintf("Database -> 'Failed to create recommendation: %v'", err))
+		return fmt.Errorf("failed to create recommendation.")
+	}
+
+	return nil
+}
