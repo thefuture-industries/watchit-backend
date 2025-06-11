3472c63e8ac3397bc14b07053e79964c660d2e62
Артём Власов <aworkprogramm@gmail.com>
2025-06-11 23:40:29 +0500

    added function NewHandler | included 'go-movie-service/internal/packages', 'net/http', 'github.com/noneandundefined/vision-go' in favourite.handler.go | added structure Handler | added type Handler | added variable favouriteRouter

--- server/movie-service/internal/module/favourite/favourite.router.go
+++ server/movie-service/internal/module/favourite/favourite.router.go
@@ -1,1 +1,17 @@
 package favourite
+
+import (
+	"go-movie-service/internal/common/middleware"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func (h Handler) RegisterRoutes(router *mux.Router) {
+	favouriteRouter := router.PathPrefix("/favourite").Subrouter()
+
+	favouriteRouter.Use(middleware.AuthMiddleware)
+
+	favouriteRouter.HandleFunc("", h.GetFavouritesHandler).Methods(http.MethodGet)
+	favouriteRouter.HandleFunc("", h.AddFavouriteHandler).Methods(http.MethodPost)
+}
