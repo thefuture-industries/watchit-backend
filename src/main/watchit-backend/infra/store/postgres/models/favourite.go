bda0e4f3ed035ecc577a12d804c2a189bdc5492b
Артём Власов <aworkprogramm@gmail.com>
2025-07-18 23:29:13 +0500

    added variables: favourites, query, favourite | if err is not equal to nil; added condition logic: errors.Is(err, sql.ErrNoRows) | included 'time' in favourite.go

--- src/main/watchit-backend/infra/store/postgres/models/favourite.go
+++ src/main/watchit-backend/infra/store/postgres/models/favourite.go
@@ -1,4 +1,12 @@
 package models
 
+import "time"
+
 type Favourite struct {
+	ID          int64     `db:"id" json:"id"`
+	CreatedAt   time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt   time.Time `db:"updated_at" json:"updated_at"`
+	UserUUID    string    `json:"user_uuid"`
+	MovieId     int       `json:"movie_id"`
+	MoviePoster string    `json:"movie_poster"`
 }
--- src/main/watchit-backend/infra/store/postgres/store/favourites.go
+++ src/main/watchit-backend/infra/store/postgres/store/favourites.go
@@ -3,7 +3,10 @@ package store
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"time"
 	"watchit/httpx/infra/logger"
+	"watchit/httpx/infra/store/postgres/models"
 )
 
 type FavouriteStore struct {
@@ -11,4 +14,27 @@ type FavouriteStore struct {
 	logger *logger.Logger
 }
 
-func (s *FavouriteStore) Get_FavouritesByUuid(ctx context.Context, uuid string) ()
+func (s *FavouriteStore) Get_FavouritesByUuid(ctx context.Context, uuid string) (*[]models.Favourite, error) {
+	favourites := []models.Favourite{}
+
+	query := `
+		SELECT id, user_uuid, movie_id, movie_poster FROM favourites WHERE user_uuid = $1
+	`
+
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	rows, err := s.db.QueryContext(ctx, query)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, nil
+		}
+
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		favourite := models.Favourite{}
+	}
+}
