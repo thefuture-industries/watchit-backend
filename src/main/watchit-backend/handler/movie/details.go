94ba537ac78f361b16fd8e7fa3033a8bdd0447a1
Артём Власов <aworkprogramm@gmail.com>
2025-07-18 22:57:59 +0500

    added variable ctx | if err is not equal to nil; if err is not equal to nil; if movie is equal to nil

--- src/main/watchit-backend/handler/movie/details.go
+++ src/main/watchit-backend/handler/movie/details.go
@@ -1,17 +1,33 @@
 package movie
 
 import (
-	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
 	"watchit/httpx/pkg/httpx"
 	"watchit/httpx/pkg/httpx/httperr"
 )
 
 func (h *Handler) GetDetailsMovieHandler(w http.ResponseWriter, r *http.Request) error {
+	ctx := r.Context()
+
 	idParam := mux.Vars(r)["id"]
 	if idParam == "" {
-		return httperr.NotFound(fmt.Sprintf("movie "))
+		return httperr.NotFound("movie with the id was not found")
+	}
+
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		return httperr.BadRequest("movie id was not an integer")
+	}
+
+	movie, err := h.Store.Movies.Get_MovieById(ctx, id)
+	if err != nil {
+		return httperr.Db(ctx, err)
+	}
+
+	if movie == nil {
+		return httperr.NotFound("movie not found")
 	}
 
 	httpx.HttpResponse(w, r, http.StatusOK, "MOVIEs")
