43c09ffa12539530245257e9092654680b8d4b76
Артём Власов <aworkprogramm@gmail.com>
2025-07-18 23:39:34 +0500

    added variable ctx | err := httpx.HttpParse(r, &payload); if err is not equal to nil; err := httpx.Validate.Struct(payload); if err is not equal to nil

--- src/main/watchit-backend/handler/favourite/add.go
+++ src/main/watchit-backend/handler/favourite/add.go
@@ -3,9 +3,23 @@ package favourite
 import (
 	"net/http"
 	"watchit/httpx/pkg/httpx"
+	"watchit/httpx/pkg/httpx/httperr"
 )
 
 func (h *Handler) AddFavouriteHandler(w http.ResponseWriter, r *http.Request) error {
+	ctx := r.Context()
+
+	var payload *FavouriteAddPayload
+
+	if err := httpx.HttpParse(r, &payload); err != nil {
+		return httperr.BadRequest(err.Error())
+	}
+
+	if err := httpx.Validate.Struct(payload); err != nil {
+		return httperr.BadRequest("not all fields are filled in")
+	}
+
+	exists, err := h.Store.Favourites.Get_FavouriteByUuidByMovieId(ctx)
 
 	httpx.HttpResponse(w, r, http.StatusOK, "FAVs")
 	return nil
