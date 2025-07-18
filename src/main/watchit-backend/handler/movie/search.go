d0cb5849907154a107871b3e2c9fc7da58a7a4c2
Артём Власов <aworkprogramm@gmail.com>
2025-07-18 22:34:46 +0500

    err := httpx.HttpParse(r, &payload); if err is not equal to nil; err := httpx.Validate.Struct(payload); if err is not equal to nil

--- src/main/watchit-backend/handler/movie/search.go
+++ src/main/watchit-backend/handler/movie/search.go
@@ -3,11 +3,22 @@ package movie
 import (
 	"net/http"
 	"watchit/httpx/pkg/httpx"
+	"watchit/httpx/pkg/httpx/httperr"
 )
 
 func (h *Handler) GetMoviesBySearchHandler(w http.ResponseWriter, r *http.Request) error {
-	var payload
+	var payload *SearchPayload
 
-	httpx.HttpResponse(w, r, http.StatusOK, "MOVIEs")
+	if err := httpx.HttpParse(r, &payload); err != nil {
+		return httperr.BadRequest(err.Error())
+	}
+
+	if err := httpx.Validate.Struct(payload); err != nil {
+		return httperr.BadRequest("not all fields are filled in")
+	}
+
+	sims :=
+
+		httpx.HttpResponse(w, r, http.StatusOK, "MOVIEs")
 	return nil
 }
