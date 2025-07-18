805ba1216b0185fa1d56d5072c949ea9d91e961f
Артём Власов <aworkprogramm@gmail.com>
2025-07-18 23:51:40 +0500

    the 'errors.log' file has been changed | the 'swagger.json' file has been changed

--- src/main/watchit-backend/cmd/server/routes.go
+++ src/main/watchit-backend/cmd/server/routes.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"watchit/httpx/handler"
 	"watchit/httpx/handler/auth"
+	"watchit/httpx/handler/favourite"
 	"watchit/httpx/handler/movie"
 	"watchit/httpx/middleware"
 )
@@ -27,6 +28,7 @@ func (s *httpServer) routes() http.Handler {
 	// routes path
 	auth.NewHandler(baseHandler).RegisterRoutes(subrouter)
 	movie.NewHandler(baseHandler).RegisterRoutes(subrouter)
+	favourite.NewHandler(baseHandler).RegisterRoutes(subrouter)
 
 	// docs
 	s.docs(subrouter)
